5ae0d35b87a81bf01b303566dc0ac87af5e6c876
Alexander Andreev <alexander.andreev@intel.com>
2024-11-12 19:17:46 +0300

    Update conda recipe (#42)
    * Update conda recipe
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.40.1, and conda-forge-pinning 2024.09.30.02.36.09
    
    * Change compiler deps
    
    * Unpin dpcpp versions
    
    * Increase sklearnex version
    
    * Sync recipe with sklearnex repo
    
    * Pin dal to 2024.7.0 until 2025 release
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.43.0, and conda-forge-pinning 2024.10.24.11.18.54
    
    * Make testing commands compatible with 2025.0.0 code
    
    * Make testing commands *fully* compatible with 2025.0.0 code
    
    * Update MPI tests command
    
    * Revert "Pin dal to 2024.7.0 until 2025 release"
    
    This reverts commit 9b2730182b03cefd0489c97ef286f02c3bd0e7ff.
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.2, and conda-forge-pinning 2024.11.08.10.03.25
    
    * Specify docker image
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.2, and conda-forge-pinning 2024.11.08.10.03.25
    
    * Pin DPC++ compiler version
    
    * Revert "Pin DPC++ compiler version"
    
    This reverts commit b2f7c892799325be723478e7f7bdd9a8cba18236.
    
    * Revert "MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.2, and conda-forge-pinning 2024.11.08.10.03.25"
    
    This reverts commit 688a205baed210af5350c0235eef42f8cfe684be.
    
    * Revert "Specify docker image"
    
    This reverts commit 843c757415014a02addc1a86dcfec2ac878a74e3.
    
    * Pin kernel-headers_linux-64
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.11.08.59.26
    
    * Revert "Revert "Specify docker image""
    
    This reverts commit 2b85aa3b0f19e7d7e48a3d837fbb18311150f316.
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.11.08.59.26
    
    * Change c_stdlib specification way
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.11.08.59.26
    
    * Remove kernel-headers_linux-64
    
    * Repin docker image
    
    * MNT: Re-rendered with conda-build 24.9.0, conda-smithy 3.44.3, and conda-forge-pinning 2024.11.11.08.59.26
    
    * Remove running dependency on dpcpp-cpp-rt
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

---
+++ recipe/run_test.bat
@@ -0,0 +1,42 @@
+@echo off
+
+rem %1 - scikit-learn-intelex repo root (should end with '\', leave empty if it's %cd% / $PWD)
+
+set exitcode=0
+
+IF NOT DEFINED PYTHON (set "PYTHON=python")
+
+%PYTHON% -c "from sklearnex import patch_sklearn; patch_sklearn()" || set exitcode=1
+
+rem Note: execute with argument --json-report as second argument
+rem in order to produce a JSON report under folder '.pytest_reports'.
+set with_json_report=0
+if "%~2"=="--json-report" (
+    set with_json_report=1
+    mkdir .pytest_reports
+    del /q .pytest_reports\*.json
+)
+
+if "%with_json_report%"=="1" (
+    %PYTHON% -m pytest --verbose -s %1tests --json-report --json-report-file=.pytest_reports\legacy_report.json || set exitcode=1
+    pytest --verbose --pyargs daal4py --json-report --json-report-file=.pytest_reports\daal4py_report.json || set exitcode=1
+    pytest --verbose --pyargs sklearnex --json-report --json-report-file=.pytest_reports\sklearnex_report.json || set exitcode=1
+    pytest --verbose --pyargs onedal --json-report --json-report-file=.pytest_reports\onedal_report.json || set exitcode=1
+    pytest --verbose %1.ci\scripts\test_global_patch.py --json-report --json-report-file=.pytest_reports\global_patching_report.json || set exitcode=1
+    if NOT EXIST .pytest_reports\legacy_report.json (
+        echo "Error: JSON report files failed to be produced."
+        set exitcode=1
+    )
+) else (
+    rem TODO: replace command below after 2025.1 release
+    rem %PYTHON% -m pytest --verbose -s %1tests || set exitcode=1
+    %PYTHON% -m unittest discover -v -s %1tests -p test*.py || set exitcode=1
+    pytest --verbose --pyargs daal4py || set exitcode=1
+    pytest --verbose --pyargs sklearnex || set exitcode=1
+    pytest --verbose --pyargs onedal || set exitcode=1
+    rem TODO: replace command below after 2025.1 release
+    rem pytest --verbose %1.ci\scripts\test_global_patch.py || set exitcode=1
+    %PYTHON% %1.ci\scripts\test_global_patch.py || set exitcode=1
+)
+
+EXIT /B %exitcode%

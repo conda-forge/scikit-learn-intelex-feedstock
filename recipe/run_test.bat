88aba9d26e68415428cd79ec6e221c96e5a6b78f
David Cortes <david.cortes@intel.com>
2025-12-11 09:38:18 +0100

    update run_test files

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,42 +1,74 @@
-@echo off
+@echo on
+rem ============================================================================
+rem Copyright 2018 Intel Corporation
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+rem ============================================================================
 
 rem %1 - scikit-learn-intelex repo root (should end with '\', leave empty if it's %cd% / $PWD)
 
 set exitcode=0
 
-IF NOT DEFINED PYTHON (set "PYTHON=python")
+setlocal enableextensions
+IF NOT DEFINED PYTHON (
+    set "PYTHON=python"
+    set NO_DIST=1
+)
+if "%PYTHON%"=="python" (
+    set NO_DIST=1
+)
+
+set SCIPY_ARRAY_API=1
 
 %PYTHON% -c "from sklearnex import patch_sklearn; patch_sklearn()" || set exitcode=1
 
+set "PYTEST_ARGS= "
+set "PYTEST_CONFIG=-c %1setup.cfg"
+
+IF DEFINED COVERAGE_RCFILE (set "PYTEST_ARGS=--cov=onedal --cov=sklearnex --cov-config=%COVERAGE_RCFILE% --cov-append --cov-branch --cov-report= %PYTEST_ARGS%")
+
 rem Note: execute with argument --json-report as second argument
 rem in order to produce a JSON report under folder '.pytest_reports'.
-set with_json_report=0
 if "%~2"=="--json-report" (
-    set with_json_report=1
+    set "PYTEST_ARGS=--json-report --json-report-file=.pytest_reports\FILENAME.json %PYTEST_ARGS%"
+    echo %PYTEST_ARGS%
     mkdir .pytest_reports
     del /q .pytest_reports\*.json
 )
 
-if "%with_json_report%"=="1" (
-    %PYTHON% -m pytest --verbose -s %1tests --json-report --json-report-file=.pytest_reports\legacy_report.json || set exitcode=1
-    pytest --verbose --pyargs daal4py --json-report --json-report-file=.pytest_reports\daal4py_report.json || set exitcode=1
-    pytest --verbose --pyargs sklearnex --json-report --json-report-file=.pytest_reports\sklearnex_report.json || set exitcode=1
-    pytest --verbose --pyargs onedal --json-report --json-report-file=.pytest_reports\onedal_report.json || set exitcode=1
-    pytest --verbose %1.ci\scripts\test_global_patch.py --json-report --json-report-file=.pytest_reports\global_patching_report.json || set exitcode=1
+echo "NO_DIST=%NO_DIST%"
+setlocal enabledelayedexpansion
+pytest %PYTEST_CONFIG% -s "%1tests" %PYTEST_ARGS:FILENAME=legacy_report% || set exitcode=1
+pytest %PYTEST_CONFIG% --pyargs daal4py %PYTEST_ARGS:FILENAME=daal4py_report% || set exitcode=1
+pytest %PYTEST_CONFIG% --pyargs sklearnex %PYTEST_ARGS:FILENAME=sklearnex_report% || set exitcode=1
+pytest %PYTEST_CONFIG% --pyargs onedal %PYTEST_ARGS:FILENAME=onedal_report% || set exitcode=1
+pytest %PYTEST_CONFIG% "%1.ci\scripts\test_global_patch.py" %PYTEST_ARGS:FILENAME=global_patching_report% || set exitcode=1
+if NOT "%NO_DIST%"=="1" (
+    %PYTHON% "%1tests\helper_mpi_tests.py"^
+        pytest -k spmd --with-mpi %PYTEST_CONFIG% -s --pyargs sklearnex %PYTEST_ARGS:FILENAME=sklearnex_spmd%
+    if !errorlevel! NEQ 0 (
+        set exitcode=1
+    )
+    %PYTHON% "%1tests\helper_mpi_tests.py"^
+        pytest --with-mpi %PYTEST_CONFIG% -s "%1tests\test_daal4py_spmd_examples.py" %PYTEST_ARGS:FILENAME=mpi_legacy%
+    if !errorlevel! NEQ 0 (
+        set exitcode=1
+    )
+)
+if "%~2"=="--json-report" (
     if NOT EXIST .pytest_reports\legacy_report.json (
         echo "Error: JSON report files failed to be produced."
         set exitcode=1
     )
-) else (
-    rem TODO: replace command below after 2025.1 release
-    rem %PYTHON% -m pytest --verbose -s %1tests || set exitcode=1
-    %PYTHON% -m unittest discover -v -s %1tests -p test*.py || set exitcode=1
-    pytest --verbose --pyargs daal4py || set exitcode=1
-    pytest --verbose --pyargs sklearnex || set exitcode=1
-    pytest --verbose --pyargs onedal || set exitcode=1
-    rem TODO: replace command below after 2025.1 release
-    rem pytest --verbose %1.ci\scripts\test_global_patch.py || set exitcode=1
-    %PYTHON% %1.ci\scripts\test_global_patch.py || set exitcode=1
 )
-
 EXIT /B %exitcode%
